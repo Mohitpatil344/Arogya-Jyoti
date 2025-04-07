15085bfee2d2550917fc75a2bae48ffeafd55bd3
Prathamesh <prathameshcode58@gmail.com>
2025-04-07 11:20:31 +0530

    scanner

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, jsonify
 
-from routes import prediction, auth, dashboard, admin, report, lifestyle
+from routes import prediction, auth, dashboard, admin, report, lifestyle, scanner  # Import all route modules
 from Database import db  # Import the database setup
 
 app = Flask(__name__ ,template_folder='templates')
@@ -30,6 +30,7 @@ app.register_blueprint(dashboard.bp)
 app.register_blueprint(admin.bp)
 app.register_blueprint(report.bp)
 app.register_blueprint(lifestyle.bp)
+app.register_blueprint(scanner.bp)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)  # Allows external access
